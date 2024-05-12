a62577737f5496536d8f93bf18db76cd1788481c
hammadx02 <hammadaali02@gmail.com>
2024-05-12 22:49:53 +0500

    changes

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_ecommerce/features/home/bloc/home_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,13 +10,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  HomeBloc homeBloc = HomeBloc();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Grocery App'),
-        centerTitle: true,
-      ),
+    return BlocConsumer<HomeBloc, HomeState>(
+      bloc: homeBloc,
+      listenWhen: (previous, current) {},
+      buildWhen: (previous, current) {},
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Grocery App'),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.favorite_border_rounded,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.shopping_bag_outlined,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
