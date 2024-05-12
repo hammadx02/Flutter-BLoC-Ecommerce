391dcfae8b4757231d8632624ac988d53f8c5e6a
hammadx02 <hammadaali02@gmail.com>
2024-05-12 23:17:15 +0500

    adding bloc features

--- lib/features/home/bloc/home_bloc.dart
+++ lib/features/home/bloc/home_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 
 
@@ -6,8 +8,25 @@ part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
-      
-    });
+   on<HomeProductWishlistButtonClickedEvent>(homeProductWishlistButtonClickedEvent);
+   on<HomeProductCartButtonClickedEvent>(homeProductCartButtonClickedEvent);
+   on<HomeProductWishlistButtonNavigateEvent>(homeProductWishlistButtonNavigateEvent);
+   on<HomeProductCartButtonNavigateEvent>(homeProductCartButtonNavigateEvent);
+  }
+
+  FutureOr<void> homeProductWishlistButtonClickedEvent(HomeProductWishlistButtonClickedEvent event, Emitter<HomeState> emit) {
+    print('Product Wishlist Button Clicked');
+  }
+
+  FutureOr<void> homeProductCartButtonClickedEvent(HomeProductCartButtonClickedEvent event, Emitter<HomeState> emit) {
+      print('Product Cart Button Clicked');
+  }
+
+  FutureOr<void> homeProductWishlistButtonNavigateEvent(HomeProductWishlistButtonNavigateEvent event, Emitter<HomeState> emit) {
+      print('Product Wishlist Navigate Clicked');
+  }
+
+  FutureOr<void> homeProductCartButtonNavigateEvent(HomeProductCartButtonNavigateEvent event, Emitter<HomeState> emit) {
+      print('Product Cart Navigate Clicked');
   }
 }
