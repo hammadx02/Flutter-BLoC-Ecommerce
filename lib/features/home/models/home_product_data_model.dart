9723b5bcea48352b1d5d3db89767bf9d74fef32d
hammadx02 <hammadaali02@gmail.com>
2024-05-13 15:27:57 +0500

    generating product model

---
+++ lib/features/home/models/home_product_data_model.dart
@@ -0,0 +1,15 @@
+class ProductModel {
+  final String id;
+  final String name;
+  final String description;
+  final double price;
+  final String imageUrl;
+
+  ProductModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+  });
+}
